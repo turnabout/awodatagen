442ca222929b56b638a7eab918983a3d28ee6ea3
Kevin Pageau <pageau.kevin@gmail.com>
2019-08-17 16:14:09 -0400

    Reindent with 4 spaces

--- awssgen.go
+++ awssgen.go
@@ -2,101 +2,101 @@
 package main
 
 import (
-	"image"
-	"image/png"
-	_ "image/png"
-	"log"
-	"os"
-	"sort"
+    "image"
+    "image/png"
+    _ "image/png"
+    "log"
+    "os"
+    "sort"
 )
 
 // Object used to store all of the game's visual data
 type visualsData struct {
-	units [][]unitFrame
+    units [][]unitFrame
 }
 
 func main() {
-	generateUnits()
+    generateUnits()
 
-	/*
-	// Grab some existing images
-	img1 := getImage(baseDirPath + "/raw_inputs/units/AntiAir/0/1.png")
-	img2 := getImage(baseDirPath + "/raw_inputs/units/AntiAir/0/2.png")
+    /*
+    // Grab some existing images
+    img1 := getImage(baseDirPath + "/raw_inputs/units/AntiAir/0/1.png")
+    img2 := getImage(baseDirPath + "/raw_inputs/units/AntiAir/0/2.png")
 
-	// Create output image
-	outputImg := image.NewRGBA(image.Rectangle{
-		Min: image.Point{X: 0, Y: 0},
-		Max: image.Point{X: 255, Y: 255},
-	})
+    // Create output image
+    outputImg := image.NewRGBA(image.Rectangle{
+        Min: image.Point{X: 0, Y: 0},
+        Max: image.Point{X: 255, Y: 255},
+    })
 
-	// Grab rectangle for both images
-	img1Rect := img1.Bounds()
-	img2Rect := img2.Bounds()
+    // Grab rectangle for both images
+    img1Rect := img1.Bounds()
+    img2Rect := img2.Bounds()
 
-	// Move image 2 to the right by 16 pixels
-	img2Rect.Min.X += 16
-	img2Rect.Max.X += 16
+    // Move image 2 to the right by 16 pixels
+    img2Rect.Min.X += 16
+    img2Rect.Max.X += 16
 
-	// Draw the two images into the output image
-	draw.Draw(outputImg, img1Rect, img1, image.Point{X: 0, Y: 0}, draw.Src)
-	draw.Draw(outputImg, img2Rect, img2, image.Point{X: 0, Y: 0}, draw.Src)
+    // Draw the two images into the output image
+    draw.Draw(outputImg, img1Rect, img1, image.Point{X: 0, Y: 0}, draw.Src)
+    draw.Draw(outputImg, img2Rect, img2, image.Point{X: 0, Y: 0}, draw.Src)
 
-	// Export the output image
-	// Use either the AWO spritesheet environment variable path or this directory as a default
-	var outputPath string
-	var envExists bool
+    // Export the output image
+    // Use either the AWO spritesheet environment variable path or this directory as a default
+    var outputPath string
+    var envExists bool
 
-	if outputPath, envExists = os.LookupEnv(ssOutputEnvVar); !envExists {
-		// Environment variable for spritesheet output doesn't exist, output it in this directory directly
-		outputPath = baseDirPath + "/" + ssOutputDefaultName
-	}
+    if outputPath, envExists = os.LookupEnv(ssOutputEnvVar); !envExists {
+        // Environment variable for spritesheet output doesn't exist, output it in this directory directly
+        outputPath = baseDirPath + "/" + ssOutputDefaultName
+    }
 
-	writeImage(outputPath, outputImg)
-	*/
+    writeImage(outputPath, outputImg)
+    */
 }
 
 // Gets the image stored at the given path
 func getImage(path string) image.Image {
-	// Load img file
-	imgFile, err := os.Open(path)
+    // Load img file
+    imgFile, err := os.Open(path)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// Decode img
-	img, _, err := image.Decode(imgFile)
+    // Decode img
+    img, _, err := image.Decode(imgFile)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	return img
+    return img
 }
 
 // Write a given image to the given path
 func writeImage(path string, outputImg image.Image) {
-	out, err := os.Create(path)
+    out, err := os.Create(path)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	if png.Encode(out, outputImg) != nil {
-		log.Fatal(err)
-	}
+    if png.Encode(out, outputImg) != nil {
+        log.Fatal(err)
+    }
 }
 
 // Get a slice of sorted keys from the given map
 func getMapSortedKeys(m map[int]string) []int {
-	sortedKeys := make([]int, 0)
+    sortedKeys := make([]int, 0)
 
-	// Add all keys from the map
-	for k := range m {
-		sortedKeys = append(sortedKeys, k)
-	}
+    // Add all keys from the map
+    for k := range m {
+        sortedKeys = append(sortedKeys, k)
+    }
 
-	sort.Ints(sortedKeys)
+    sort.Ints(sortedKeys)
 
-	return sortedKeys
+    return sortedKeys
 }
--- genunits.go
+++ genunits.go
@@ -2,69 +2,69 @@
 package main
 
 import (
-	"fmt"
-	"image"
-	"io/ioutil"
-	"log"
-	"os"
+    "fmt"
+    "image"
+    "io/ioutil"
+    "log"
+    "os"
 )
 
 // Unit types "enum"
 type unitType uint8
 
 const (
-	Infantry unitType = iota
-	Mech
-	Recon
-	Tank
-	MdTank
-	NeoTank
-	APC
-	Artillery
-	Rockets
-	Missiles
-	AntiAir
-	Battleship
-	Cruiser
-	Lander
-	Sub
-	Fighter
-	Bomber
-	BattleCopter
-	TransportCopter
+    Infantry unitType = iota
+    Mech
+    Recon
+    Tank
+    MdTank
+    NeoTank
+    APC
+    Artillery
+    Rockets
+    Missiles
+    AntiAir
+    Battleship
+    Cruiser
+    Lander
+    Sub
+    Fighter
+    Bomber
+    BattleCopter
+    TransportCopter
 )
 
 // Unit variations "enum"
 type unitVariation uint8
 
 const (
-	OS unitVariation = iota
-	BM
-	GE
-	YC
-	BH
+    OS unitVariation = iota
+    BM
+    GE
+    YC
+    BH
 )
 
 // Unit animations "enum"
 type unitAnimation uint8
 
 const (
-	Idle unitAnimation = iota
-	Right
-	Up
-	Down
+    Idle unitAnimation = iota
+    Right
+    Up
+    Down
 )
 
 // Used to store a unit's frame's visual data within the game's sprite sheet
 type unitFrame struct {
-	x, y, w, h int
+    x, y, w, h int
 }
 
 // Used to store a unit's animation image data (image/width/height)
 type unitImg struct {
-	img image.Image
-	w int
-	h int
+    img image.Image
+    w int
+    h int
 }
 
 // Data on every single unit image
@@ -75,116 +75,116 @@ var unitsImgData[][][][]unitImg
 
 // Every unit type names as indexes, and their corresponding numbered values
 var unitTypes = map[int]string {
-	int(Infantry):        "Infantry",
-	int(Mech):            "Mech",
-	int(Recon):           "Recon",
-	int(Tank):            "Tank",
-	int(MdTank):          "MdTank",
-	int(NeoTank):         "NeoTank",
-	int(APC):             "APC",
-	int(Artillery):       "Artillery",
-	int(Rockets):         "Rockets",
-	int(Missiles):        "Missiles",
-	int(AntiAir):         "AntiAir",
-	int(Battleship):      "Battleship",
-	int(Cruiser):         "Cruiser",
-	int(Lander):          "Lander",
-	int(Sub):             "Sub",
-	int(Fighter):         "Fighter",
-	int(Bomber):          "Bomber",
-	int(BattleCopter):    "BattleCopter",
-	int(TransportCopter): "TransportCopter",
+    int(Infantry):        "Infantry",
+    int(Mech):            "Mech",
+    int(Recon):           "Recon",
+    int(Tank):            "Tank",
+    int(MdTank):          "MdTank",
+    int(NeoTank):         "NeoTank",
+    int(APC):             "APC",
+    int(Artillery):       "Artillery",
+    int(Rockets):         "Rockets",
+    int(Missiles):        "Missiles",
+    int(AntiAir):         "AntiAir",
+    int(Battleship):      "Battleship",
+    int(Cruiser):         "Cruiser",
+    int(Lander):          "Lander",
+    int(Sub):             "Sub",
+    int(Fighter):         "Fighter",
+    int(Bomber):          "Bomber",
+    int(BattleCopter):    "BattleCopter",
+    int(TransportCopter): "TransportCopter",
 }
 
 // Every unit variation names as indexes, and their corresponding numbered values
 var unitVariations = map[int]string {
-	int(OS): "OS",
-	int(BM): "BM",
-	int(GE): "GE",
-	int(YC): "YC",
-	int(BH): "BH",
+    int(OS): "OS",
+    int(BM): "BM",
+    int(GE): "GE",
+    int(YC): "YC",
+    int(BH): "BH",
 }
 
 // Every unit animation names as indexes, and their corresponding numbered values
 var unitAnimations = map[int]string {
-	int(Idle):  "Idle",
-	int(Right): "Right",
-	int(Up):    "Up",
-	int(Down):  "Down",
+    int(Idle):  "Idle",
+    int(Right): "Right",
+    int(Up):    "Up",
+    int(Down):  "Down",
 }
 
 func init() {
-	// Initialize unitsImgData slice
-	unitsImgData = make([][][][]unitImg, len(unitTypes))
+    // Initialize unitsImgData slice
+    unitsImgData = make([][][][]unitImg, len(unitTypes))
 }
 
 // Generate units' spritesheet & visuals data
 func generateUnits() {
-	gatherUnitImgData()
+    gatherUnitImgData()
 
-	fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][0].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][0].h)
-	fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][1].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][1].h)
-	fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][2].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][2].h)
+    fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][0].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][0].h)
+    fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][1].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][1].h)
+    fmt.Printf("%d %d\n", unitsImgData[int(Infantry)][int(OS)][int(Idle)][2].w, unitsImgData[int(Infantry)][int(OS)][int(Idle)][2].h)
 }
 
 // Gathers data on every single image, filling out "unitsImgData"
 func gatherUnitImgData() {
-	// Get path of base directory containing unit images
-	var unitsDirPath string = baseDirPath + imageInputsDirName + unitsDirName + "/"
+    // Get path of base directory containing unit images
+    var unitsDirPath string = baseDirPath + imageInputsDirName + unitsDirName + "/"
 
-	// Get sorted keys for every unit map
-	sortedUnitKeys := getMapSortedKeys(unitTypes)
-	sortedVarKeys := getMapSortedKeys(unitVariations)
-	sortedAnimKeys := getMapSortedKeys(unitAnimations)
+    // Get sorted keys for every unit map
+    sortedUnitKeys := getMapSortedKeys(unitTypes)
+    sortedVarKeys := getMapSortedKeys(unitVariations)
+    sortedAnimKeys := getMapSortedKeys(unitAnimations)
 
-	// Loop every unit in order of values from unitTypes
-	for unitKey := range sortedUnitKeys {
-		var unitDirPath string = unitsDirPath + unitTypes[unitKey] + "/"
+    // Loop every unit in order of values from unitTypes
+    for unitKey := range sortedUnitKeys {
+        var unitDirPath string = unitsDirPath + unitTypes[unitKey] + "/"
 
-		// Loop every unit variation in order of values from unitVariations
-		for varKey := range sortedVarKeys {
-			var varDirPath string = unitDirPath + unitVariations[varKey] + "/"
+        // Loop every unit variation in order of values from unitVariations
+        for varKey := range sortedVarKeys {
+            var varDirPath string = unitDirPath + unitVariations[varKey] + "/"
 
-			// Ignore this variation if it does not exist on this unit
-			if _, err := os.Stat(varDirPath); os.IsNotExist(err) {
-				break
-			}
+            // Ignore this variation if it does not exist on this unit
+            if _, err := os.Stat(varDirPath); os.IsNotExist(err) {
+                break
+            }
 
-			// Add array for this variation to unitsImgData
-			unitsImgData[unitKey] = append(unitsImgData[unitKey], [][]unitImg{})
+            // Add array for this variation to unitsImgData
+            unitsImgData[unitKey] = append(unitsImgData[unitKey], [][]unitImg{})
 
-			// Loop every variation animation
-			for animKey := range sortedAnimKeys {
-				// Add array for this animation to unitsImgData
-				unitsImgData[unitKey][varKey] = append(unitsImgData[unitKey][varKey], []unitImg{})
+            // Loop every variation animation
+            for animKey := range sortedAnimKeys {
+                // Add array for this animation to unitsImgData
+                unitsImgData[unitKey][varKey] = append(unitsImgData[unitKey][varKey], []unitImg{})
 
-				// Gather data from this animation's images
-				gatherAnimationData(unitKey, varKey, animKey, varDirPath + unitAnimations[animKey] + "/")
-			}
-		}
-	}
+                // Gather data from this animation's images
+                gatherAnimationData(unitKey, varKey, animKey, varDirPath + unitAnimations[animKey] + "/")
+            }
+        }
+    }
 }
 
 // Process a unit animation's directory, gathering all of its images' data for unitsImgData
 func gatherAnimationData(unitKey int, varKey int, animKey int, animDir string) {
-	// Loop imageNames
-	files, err := ioutil.ReadDir(animDir)
+    // Loop imageNames
+    files, err := ioutil.ReadDir(animDir)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// Gather every image's data
-	for _, file := range files {
-		imageObj := getImage(animDir + file.Name())
+    // Gather every image's data
+    for _, file := range files {
+        imageObj := getImage(animDir + file.Name())
 
-		unitsImgData[unitKey][varKey][animKey] = append(
-			unitsImgData[unitKey][varKey][animKey],
-			unitImg {
-				img: imageObj,
-				w: imageObj.Bounds().Max.X,
-				h: imageObj.Bounds().Max.Y,
-			},
-		)
-	}
+        unitsImgData[unitKey][varKey][animKey] = append(
+            unitsImgData[unitKey][varKey][animKey],
+            unitImg {
+                img: imageObj,
+                w: imageObj.Bounds().Max.X,
+                h: imageObj.Bounds().Max.Y,
+            },
+        )
+    }
 }
