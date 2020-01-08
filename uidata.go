2db4a9534fca186553a8308321ab9e26c3132001
Kevin Pageau <pageau.kevin@gmail.com>
2020-01-08 14:41:55 -0500

    Add tile cursor "X" version

--- uidata.go
+++ uidata.go
@@ -8,6 +8,7 @@ type UIElement uint8
 
 const(
     TileCursor UIElement = iota
+    TileCursorX
     StarSm
     StarLg
 
@@ -21,6 +22,7 @@ const UIElementCount = UIElementLast + 1
 // Map for looking up a Ui Element using its corresponding full string
 var UIElementsReverseStrings = map[string]UIElement {
     "TileCursor": TileCursor,
+    "TileCursorX": TileCursorX,
     "StarSm": StarSm,
     "StarLg": StarLg,
 }
