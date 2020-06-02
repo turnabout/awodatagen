fb2942749f8b8a326fd350923a5565ccbccd3ac2
Kevin Pageau <pageau.kevin@gmail.com>
2020-06-01 21:50:16 -0400

    Add start of extra units data to assets & enums

--- unitdata.go
+++ unitdata.go
@@ -113,22 +113,69 @@ type MovementType uint8
 const (
     Foot MovementType = iota
     HeavyFoot
+    Treads
+    Tires
+    Air
+    Naval
 )
 
 // Map for looking up a Weapon Type using its corresponding string
 var MovementTypeReverseStrings = map[string]MovementType {
     "Foot": Foot,
     "HeavyFoot": HeavyFoot,
+    "Treads": Treads,
+    "Tires": Tires,
+    "Air": Air,
+    "Naval": Naval,
 }
 
 // Weapon type enum
 type WeaponType uint8
 
 const (
-    MachineGun WeaponType = iota
+    WeaponMachineGunMk1 WeaponType = iota // Infantry secondary
+    WeaponMachineGunMk2                   // Mech/BattleCopter secondary
+    WeaponMachineGunMk3                   // Recon/Tank/MdTank secondary
+    WeaponMachineGunMk4                   // NeoTank secondary
+
+    WeaponCannonRangedMk1                 // Artillery primary
+    WeaponCannonRangedMk2                 // Battleship primary
+    WeaponCannonMk1                       // Tank primary
+    WeaponCannonMk2                       // MdTank primary
+    WeaponCannonMk3                       // NeoTank primary
+
+    WeaponMissilesRangedMk1               // Missiles primary
+    WeaponMissilesMk1                     // BattleCopter primary
+    WeaponMissilesMk2                     // Fighter primary
+    WeaponMissilesMk3                     // Cruiser primary
+
+    WeaponBombs                           // Bomber primary
+    WeaponRockets                         // Rockets primary
+    WeaponTorpedoes                       // Sub primary
+    WeaponAntiAirGun                      // Cruiser secondary
+    WeaponBazooka                         // Mech primary
+    WeaponVulcan                          // AntiAir primary
 )
 
 // Map for looking up a Weapon Type using its corresponding string
 var WeaponTypeReverseStrings = map[string]WeaponType {
-    "MachineGun": MachineGun,
+    "MachineGunMk1":     WeaponMachineGunMk1,
+    "MachineGunMk2":     WeaponMachineGunMk2,
+    "MachineGunMk3":     WeaponMachineGunMk3,
+    "MachineGunMk4":     WeaponMachineGunMk4,
+    "CannonRangedMk1":   WeaponCannonRangedMk1,
+    "CannonRangedMk2":   WeaponCannonRangedMk2,
+    "CannonMk1":         WeaponCannonMk1,
+    "CannonMk2":         WeaponCannonMk2,
+    "CannonMk3":         WeaponCannonMk3,
+    "MissilesRangedMk1": WeaponMissilesRangedMk1,
+    "MissilesMk1":       WeaponMissilesMk1,
+    "MissilesMk2":       WeaponMissilesMk2,
+    "MissilesMk3":       WeaponMissilesMk3,
+    "Bombs":             WeaponBombs,
+    "Rockets":           WeaponRockets,
+    "Torpedoes":         WeaponTorpedoes,
+    "AntiAirGun":        WeaponAntiAirGun,
+    "Bazooka":           WeaponBazooka,
+    "Vulcan":            WeaponVulcan,
 }
